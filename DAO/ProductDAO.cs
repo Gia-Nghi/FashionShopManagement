a30e4b6ad3c41007cab408e569dbac832acbb212
2ThuanCr <2ThuanCr03@gmail.com>
2024-10-22 23:33:55 +0700

    update DAO

---
+++ DAO/ProductDAO.cs
@@ -0,0 +1,128 @@
+﻿using DTO;
+using System;
+using System.Data;
+
+namespace DAO
+{
+    public class ProductDAO
+    {
+        private static ProductDAO instance;
+
+        public static ProductDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ProductDAO();
+                return ProductDAO.instance;
+            }
+        }
+
+        private ProductDAO() { }
+
+        public DataTable GetAllProduct()
+        {
+            string query = "USP_GetAllProduct";
+            try
+            {
+                return DataProvider.Instance.ExecuteQuery(query);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataTable GetListProductByCategory(string category)
+        {
+            string query = "USP_GetListProductByCategory @Category";
+            try
+            {
+                return DataProvider.Instance.ExecuteQuery(query, new object[] { category });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataTable SearchProductByName(string name)
+        {
+            string query = string.Format("USP_SearchProductByName @Name");
+            try
+            {
+                return DataProvider.Instance.ExecuteQuery(query, new object[] { name });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool InsertProduct(Product newProduct)
+        {
+            string query = string.Format("USP_InsertProduct @Name , @Price, @Discount, @Category , @Quantity, @Size, @Image");
+            int result;
+            try
+            {
+                result = DataProvider.Instance.ExecuteNonQuery(query,
+                    new object[]
+                    {
+                        newProduct.Name,
+                        newProduct.Price,
+                        newProduct.Discount,
+                        newProduct.Category,
+                        newProduct.Quantity,
+                        newProduct.Size,
+                        newProduct.Image,
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result > 0;
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            string query = string.Format("USP_UpdateFood @Id , @Name , @Price, @Discount, @Category , @Quantity, @Size, @Image");
+            int result;
+            try
+            {
+                result = DataProvider.Instance.ExecuteNonQuery(query,
+                    new object[] { 
+                        product.Id, 
+                        product.Name,
+                        product.Price,
+                        product.Discount,
+                        product.Category, 
+                        product.Quantity,
+                        product.Size,
+                        product.Image
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result > 0;
+        }
+
+        public bool DeleteProduct(int Id)
+        {
+            int result;
+            try
+            {
+                result = DataProvider.Instance.ExecuteNonQuery("USP_DeleteProduct @Id", new object[] { Id });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result > 0;
+        }
+    }
+}
---
+++ DTO/Product.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+
+namespace DTO
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public string Image { get; set; }
+
+        public Product(string name, decimal price, decimal discount, string category, int quantity, string size, string image)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Discount = discount;
+            this.Category = category;
+            this.Quantity = quantity;
+            this.Size = size;
+            this.Image = image;
+        }
+        public Product(int id, string name, decimal price, decimal discount, string category, int quantity, string size, string image)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Discount = discount;
+            this.Category = category;
+            this.Quantity = quantity;
+            this.Size = size;
+            this.Image = image;
+        }
+
+        public Product(DataRow row)
+        {
+            this.Id = (int)row["Id"];
+            this.Name = row["Name"].ToString();
+            this.Price = (decimal)row["Price"];
+            this.Discount = (decimal)row["Discount"];
+            this.Category = (string)row["Category"];
+            this.Quantity = (int)row["Quantity"];
+            this.Size = (string)row["Size"];
+            this.Image = (string)row["Image"];
+        }
+    }
+}
