8ec9bc055baa0472253eee486958cb97a8d83411
KhoanVo1407 <phunkhoanvo123@gmail.com>
2024-11-10 17:12:39 +0700

    a

--- DAO/ChiTietSanPhamDao.cs
+++ DAO/ChiTietSanPhamDao.cs
@@ -1,24 +1,11 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
+using DAO;
 
 namespace DAO
 {
     public class ChiTietSanPhamDao
     {
-        private static ChiTietSanPhamDao instance;
-
-        public static ChiTietSanPhamDao Instance
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new ChiTietSanPhamDao();
-                return instance;
-            }
-        }
-
-        // Phương thức lấy chi tiết sản phẩm theo mã sản phẩm
         public DataTable GetChiTietSanPhamByMaSP(string maSP)
         {
             string query = "SELECT * FROM ChiTietSanPhamView WHERE MaSP = @MaSP";
--- GUI/fChiTietSanPham.cs
+++ GUI/fChiTietSanPham.cs
@@ -1,52 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
 
 namespace GUI
 {
     public partial class fChiTietSanPham : Form
     {
+        private ChiTietSanPhamBUS bll;
+
         public fChiTietSanPham(string maSP)
         {
             InitializeComponent();
+            bll = new ChiTietSanPhamBUS();
             LoadSanPham(maSP);
         }
 
         private void LoadSanPham(string maSP)
         {
-            using (SqlConnection conn = new SqlConnection(@"Data Source=ASUS-TUFGAMING;Initial Catalog=KVShop;Integrated Security=True;Encrypt=False"))
+            DataTable sanPhamData = bll.GetChiTietSanPhamByMaSP(maSP);
+            if (sanPhamData.Rows.Count > 0)
             {
-                conn.Open();
-                string query = "SELECT * FROM ChiTietSanPhamView WHERE MaSP = @MaSP";
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@MaSP", maSP);
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            label_TenSanPham.Text = reader["TenSP"].ToString();
-                            labelGiaSP.Text = $"{reader["DonGia"]:C} VNĐ";
-                            labelSoLuong.Text = $"Số lượng: {reader["SL"]}";
-                            labelSIZE.Text = $"Kích cỡ: {reader["Size"]}";
-                            labelLoaiSP.Text = $"Loại sản phẩm: {reader["TenLoaiSP"]}";
-                            labelNhaCungCap.Text = $"Nhà cung cấp: {reader["NhaCungCap"]}";
-                        }
-                    }
-                }
+                DataRow row = sanPhamData.Rows[0];
+                label_TenSanPham.Text = row["TenSP"].ToString();
+                labelGiaSP.Text = $"{row["DonGia"]:C} VNĐ";
+                labelSoLuong.Text = $"Số lượng: {row["SL"]}";
+                labelSIZE.Text = $"Kích cỡ: {row["Size"]}";
+                labelLoaiSP.Text = $"Loại sản phẩm: {row["TenLoaiSP"]}";
+                labelNhaCungCap.Text = $"Nhà cung cấp: {row["NhaCungCap"]}";
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy sản phẩm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         private void buttonChinhSua_Click(object sender, EventArgs e)
         {
-
         }
 
         private void buttonXoa_Click(object sender, EventArgs e)
